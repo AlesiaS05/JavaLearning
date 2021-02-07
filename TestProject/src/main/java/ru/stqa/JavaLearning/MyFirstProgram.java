4a79ffcfdffc2e56b36436db1aea2900894151e6
AlesiaS05 <smirnovaalesia5@gmail.com>
2021-02-07 13:37:12 -0500

    added several funcrions

--- TestProject/src/main/java/ru/stqa/JavaLearning/MyFirstProgram.java
+++ TestProject/src/main/java/ru/stqa/JavaLearning/MyFirstProgram.java
@@ -2,7 +2,24 @@ package ru.stqa.JavaLearning;
 
 public class MyFirstProgram {
   public static void main(String[] args) {
-    System.out.println("Hello, world!");
+    hello("world");
+    hello("Alesia");
+    double l = 5;
+      System.out.println( "ploschad' kvadrata so storonoi " + l + "=" + area(l));
+    double a = 4;
+    double b = 3;
+      System.out.println("ploschad' praymougolnika so stotonami " + a + " i " + b + " = " + area(a,b));
+
+  }
+  public static void hello(String somebody) {
+
+      System.out.println("Hello, " + somebody + "!");
+  }
+  public static double area (double len){
+      return len * len;
+  }
+  public static double area (double a, double b){
+      return a * b;
   }
 
 }	
